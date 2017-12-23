be6c3d2376fb0fb37c84765d52e9f617e2490b5c
Ian Miller <ircone@yahoo.com>
2017-12-23 15:27:37 -0800

    oops

--- config.php
+++ config.php
@@ -5,5 +5,3 @@ define('SAVE_PATH', '~/Downloads');
 define('QUEUE_FILE_NAME', 'queue.txt');
 define('QUEUE_FILE_WEB_ADDRESS', 'http://foo.com/' . QUEUE_FILE_NAME);
 
-define('SAVE_PATH', '/Volumes/videos/more videos/movies');
-define('QUEUE_FILE_WEB_ADDRESS', 'http://yawmp.com/projects/tor_remote/queue.txt' . QUEUE_FILE_NAME);
